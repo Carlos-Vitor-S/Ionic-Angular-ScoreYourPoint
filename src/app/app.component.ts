6497a4e82840d2795bbfa83b5fe37f0b829f5fd7
Bruno Batista Santos <brunobatysta.fla@gmail.com>
2023-11-15 15:21:27 -0300

    cria observable para detectar mudanças

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,57 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['app.component.scss'],
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   displayName = null
+  isLoggedIn = false;
+  private subscription: Subscription;
 
 
   constructor(
-  ) {}
+    private authService: AuthService
+  ) {
+    this.subscription = this.authService.isLoggedIn$.subscribe((isLoggedIn) => {
+      this.isLoggedIn = isLoggedIn;
+      /*let user = JSON.parse(localStorage.getItem('user'));
+      console.log(user);
+      if (this.isLoggedIn == true && user != null) {
+        let name = user.displayName;
+        let firstName = name.split(' ')[0];
+        this.displayName = firstName;
+        localStorage.setItem('isLoggedIn', true.toString());
+        console.log(firstName)
+      }*/
+    });
+    this.subscription = this.authService.nameUserLoggedIn$.subscribe((displayName) => {
+      this.displayName = displayName;
+    });
+  }
 
   ngOnInit() {
-    let data = localStorage.getItem('user')
-    let user = JSON.parse(data!)
-    this.displayName = user.displayName
+    let t = JSON.parse(localStorage.getItem('isLoggedIn'));
+    if (t == true) {
+      this.isLoggedIn = t
+      this.setName();
+    }
+  }
+
+
+
+  ngOnDestroy() {
+    // Certifique-se de cancelar a inscrição para evitar vazamentos de memória
+    this.subscription.unsubscribe();
+  }
+
+  setName() {
+    let user = JSON.parse(localStorage.getItem('user'))
+    if (this.isLoggedIn == true && user != null) {
+      let name = user.displayName;
+      let firstName = name.split(' ')[0];
+      this.displayName = firstName;
+      localStorage.setItem('isLoggedIn', true.toString());
+      console.log(firstName)
+    }
   }
 
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,15 +2,48 @@ import { Injectable, NgZone } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private authentication: AngularFireAuth) {}
+  private firstName: string
+  private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this.isLoggedInSubject.asObservable();
+
+  private nameUserSubject = new BehaviorSubject<string>(null);
+  nameUserLoggedIn$ = this.nameUserSubject.asObservable();
+
+  constructor(
+    private authentication: AngularFireAuth,
+    private storageService: StorageService,
+    private router: Router
+  ) { }
 
   signInWithEmail(email: string, senha: string) {
-    return this.authentication.signInWithEmailAndPassword(email, senha);
+    this.authentication.signInWithEmailAndPassword(email, senha)
+      .then((loginCredential) => {
+        let userName = loginCredential.user.displayName.split(' ')[0];
+        this.firstName = userName;
+        loginCredential.user?.getIdToken().then((token) => {
+          let accessToken = token;
+          this.storageService.setLocalstorage('token', accessToken);
+          localStorage.setItem(
+            'user',
+            JSON.stringify(loginCredential.user)
+          );
+          this.router.navigate(['home']);
+        });
+        this.isLoggedInSubject.next(true);
+        this.nameUserSubject.next(this.firstName);
+        console.log('login sucesso', this.firstName);
+        localStorage.setItem('isLoggedIn', true.toString());
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
   }
 
   registerWithEmail(email: string, senha: string) {
